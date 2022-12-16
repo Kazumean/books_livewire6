4b589fcc66fff988435cbba78bc233452383eaf8
Kazumean <kazumin.angelav@gmail.com>
2022-12-16 15:07:11 +0900

    プロジェクト切り替えのためのコミット。

--- app/Http/Livewire/BookIndex.php
+++ app/Http/Livewire/BookIndex.php
@@ -3,9 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Book;
 
 class BookIndex extends Component
 {
+    // モーダルウィンドウ
+    public $liveModal = false;
+    // タイトル
+    public $title;
+    // 画像
+    public $image;
+    // 価格
+    public $price;
+    // 詳細
+    public $description;
+
     public function render()
     {
         return view('livewire.book-index');
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,3 +1,11 @@
 <div  class=”max-w-6xl mx-auto”>
     book index
+
+    <x-jet-dialog-modal wire:model="liveModal">
+        <x-slot name="title"><h2 class="text-green-600">登録</h2></x-slot>
+        <x-slot name="content">内容
+        </x-slot>
+        <x-slot name="footer">フッター
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
